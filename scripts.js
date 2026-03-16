09bbcace4f8ced89eaeeb0cc1ee731e23f09726f
lucky <altlucky120@gmail.com>
2026-03-16 15:22:26 +0530

    check

---
+++ scripts.js
@@ -0,0 +1,14 @@
+const observer = new IntersectionObserver((entries)=>{
+entries.forEach(entry=>{
+if(entry.isIntersecting){
+entry.target.classList.add("visible");
+}
+});
+}
+,{
+threshold:0.35
+});
+
+document.querySelectorAll(".animate").forEach(el=>{
+observer.observe(el);
+});
